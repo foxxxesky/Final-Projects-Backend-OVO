b951198a57c7e7b469a3bf35b4dcafd8260dc0fd
foxxxesky <abdurrahman1467@gmail.com>
2023-05-20 00:46:27 +0700

    update api

--- controller/Transaction/Transaction.Controller.js
+++ controller/Transaction/Transaction.Controller.js
@@ -31,7 +31,7 @@ exports.show = async (req, res) => {
       {
         model: User,
         attributes: {
-          exclude: ['security_code', 'email_verified', 'phone_verified', 'photo', 'createdAt', 'updatedAt']
+          exclude: ['security_code', 'email_verified', 'phone_verified', 'createdAt', 'updatedAt']
         },
         as: 'user_transaction'
       },
@@ -64,49 +64,56 @@ exports.charge = async (req, res, next) => {
   const decoded = jwt.verify(token, process.env.ACCESS_TOKEN)
 
   const wallet = await Wallet.findOne({ where: { user_id: decoded.user.id } })
-
-  // transaction
-  req.body.transaction_details.order_id = uuid.v4()
-
-  // customer
-  req.body.customer_details = {
-    first_name: decoded.user.name,
-    phone: decoded.user.phone,
-    email: decoded.user.email
+  if (!wallet) {
+    return res.status(400).json({ message: 'wallet not found!' })
   }
 
-  // custom expiry
-  req.body.custom_expiry = {
-    expiry_duration: 5, // set to 5 minutes
-    unit: 'minute'
+  try {
+    // transaction
+    req.body.transaction_details.order_id = uuid.v4()
+
+    // customer
+    req.body.customer_details = {
+      first_name: decoded.user.name,
+      phone: decoded.user.phone,
+      email: decoded.user.email
+    }
+
+    // custom expiry
+    req.body.custom_expiry = {
+      expiry_duration: 5, // set to 5 minutes
+      unit: 'minute'
+    }
+
+    coreApi.charge(req.body)
+      .then(async (response) => {
+        const payload = {
+          id: response.transaction_id,
+          user_id: decoded.user.id,
+          wallet_id: wallet.id,
+          transaction_method_id: null,
+          amount: parseFloat(response.gross_amount),
+          notes: null,
+          transaction_type: 'debit',
+          transaction_status: response.transaction_status
+        }
+
+        const transaction = await WalletTransaction.create(payload)
+
+        res.status(200).json({
+          midtrans: response,
+          user_transaction: transaction
+        })
+      })
+      .catch((err) => {
+        console.log(err.message)
+        res.status(200).json({
+          data: err.message
+        })
+      })
+  } catch (error) {
+    return res.status(400).json({ message: 'there is an error!' })
   }
-
-  coreApi.charge(req.body)
-    .then(async (response) => {
-      const payload = {
-        id: response.transaction_id,
-        user_id: decoded.user.id,
-        wallet_id: wallet.id,
-        transaction_method_id: null,
-        amount: parseFloat(response.gross_amount),
-        notes: null,
-        transaction_type: 'debit',
-        transaction_status: response.transaction_status
-      }
-
-      const transaction = await WalletTransaction.create(payload)
-
-      res.status(200).json({
-        midtrans: response,
-        user_transaction: transaction
-      })
-    })
-    .catch((err) => {
-      console.log(err.message)
-      res.status(200).json({
-        data: err.message
-      })
-    })
 }
 
 exports.status = async (req, res) => {
@@ -115,72 +122,84 @@ exports.status = async (req, res) => {
   const decoded = jwt.verify(token, process.env.ACCESS_TOKEN)
 
   const wallet = await Wallet.findOne({ where: { user_id: decoded.user.id } })
+
+  if (!wallet) {
+    return res.status(400).json({ message: 'wallet not found!' })
+  }
+
   const previousAmount = wallet.balance
 
   const userTransaction = await WalletTransaction.findOne({ where: { id: req.query.transaction_id } })
 
-  coreApi.transaction.status(req.query.transaction_id)
-    .then(async (response) => {
-      const transactionStatus = response.transaction_status
+  try {
+    coreApi.transaction.status(req.query.transaction_id)
+      .then(async (response) => {
+        const transactionStatus = response.transaction_status
 
-      if (transactionStatus === 'pending') {
-        const payload = {
-          transaction_status: response.transaction_status
+        if (transactionStatus === 'pending') {
+          const payload = {
+            transaction_status: response.transaction_status
+          }
+          await userTransaction.update(payload)
+        } else if (transactionStatus === 'settlement') {
+          const transactionPayload = {
+            transaction_status: 'done'
+          }
+
+          const newAmount = previousAmount + parseFloat(response.gross_amount)
+
+          const walletPayload = {
+            balance: newAmount
+          }
+
+          await userTransaction.update(transactionPayload)
+          await wallet.update(walletPayload)
+        } else if (transactionStatus === 'expire') {
+          const payload = {
+            transaction_status: 'failed'
+          }
+
+          await userTransaction.update(payload)
         }
-        await userTransaction.update(payload)
-      } else if (transactionStatus === 'settlement') {
-        const transactionPayload = {
-          transaction_status: 'done'
-        }
 
-        const newAmount = previousAmount + parseFloat(response.gross_amount)
-
-        const walletPayload = {
-          balance: newAmount
-        }
-
-        await userTransaction.update(transactionPayload)
-        await wallet.update(walletPayload)
-        console.log(transactionStatus)
-      } else if (transactionStatus === 'expire') {
-        const payload = {
-          transaction_status: 'failed'
-        }
-
-        await userTransaction.update(payload)
-      }
-
-      res.status(200).json({
-        midtrans: response,
-        user_transaction: userTransaction
+        res.status(200).json({
+          midtrans: response,
+          user_transaction: userTransaction
+        })
       })
-    })
-    .catch((err) => {
-      res.status(200).json({
-        data: err.message
+      .catch((err) => {
+        res.status(400).json({
+          data: err.message
+        })
       })
-    })
+  } catch (error) {
+    return res.status(400).json({ message: 'there is an error!' })
+  }
 }
 
 exports.cancel = async (req, res) => {
-  const userTransaction = await WalletTransaction.findOne({ where: { id: req.query.transaction_id } })
-  coreApi.transaction.cancel(req.query.transaction_id)
-    .then(async (response) => {
-      const payload = {
-        transaction_status: 'canceled'
-      }
-      userTransaction.update(payload)
+  try {
+    const userTransaction = await WalletTransaction.findOne({ where: { id: req.query.transaction_id } })
+    coreApi.transaction.cancel(req.query.transaction_id)
+      .then(async (response) => {
+        const payload = {
+          transaction_status: 'canceled'
+        }
+        userTransaction.update(payload)
 
-      res.status(200).json({
-        midtrans: response,
-        user_transaction: userTransaction
+        res.status(200).json({
+          midtrans: response,
+          user_transaction: userTransaction
+        })
       })
-    })
-    .catch((err) => {
-      res.status(200).json({
-        data: err.message
+      .catch((err) => {
+        res.status(200).json({
+          data: err.message
+        })
       })
-    })
+  } catch (error) {
+    return res.status(400).json({ message: 'there is an error!' })
+  }
 }
 
 exports.payment = async (req, res, next) => {
