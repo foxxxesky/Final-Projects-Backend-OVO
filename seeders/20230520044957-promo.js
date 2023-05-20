437d64ea6ed8f676ef0ace31d851f289a5d6a356
foxxxesky <abdurrahman1467@gmail.com>
2023-05-20 12:13:57 +0700

    create promos seed

--- seeders/20230520044957-promo.js
+++ seeders/20230520044957-promo.js
@@ -1,25 +1,42 @@
-'use strict';
+'use strict'
+
+const uuid = require('uuid')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('promos', [
+      {
+        id: uuid.v4(),
+        name: 'Promo Discount 5%',
+        description: 'Minimal Pembelian Rp. 20.000 Untuk Menggunakan Promo ini',
+        discount: 0.05,
+        expired_at: new Date(Date.now() + 14 * 24 * 60 * 60 * 1000), // Set the expiration date to 14 days from now,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: uuid.v4(),
+        name: 'Promo Discount 10%',
+        description: 'Minimal Pembelian Rp. 50.000 Untuk Menggunakan Promo ini',
+        discount: 0.1,
+        expired_at: new Date(Date.now() + 14 * 24 * 60 * 60 * 1000), // Set the expiration date to 14 days from now,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: uuid.v4(),
+        name: 'Promo Discount 20%',
+        description: 'Minimal Pembelian Rp. 100.000 Untuk Menggunakan Promo ini',
+        discount: 0.2,
+        expired_at: new Date(Date.now() + 14 * 24 * 60 * 60 * 1000), // Set the expiration date to 14 days from now,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('promos', null, {})
   }
-};
+}
