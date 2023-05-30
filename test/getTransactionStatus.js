ae6f314d089722d9fc205b9d5734908cd3e1150f
foxxxesky <abdurrahman1467@gmail.com>
2023-05-30 10:47:23 +0700

    Update getTransactionStatus.js

--- test/getTransactionStatus.js
+++ test/getTransactionStatus.js
@@ -7,26 +7,27 @@ export const options = {
     http_req_failed: ['rate<0.2'] // http errors should be less than 20%
   },
   stages: [
+    // { duration: '1m', target: 50 }
     // 50
-    // { duration: '10s', target: 25 },
-    // { duration: '20s', target: 50 },
-    // { duration: '20s', target: 50 },
-    // { duration: '10s', target: 35 }
-    // 100
-    // { duration: '10s', target: 50 },
+    { duration: '10s', target: 30 },
+    { duration: '20s', target: 50 },
+    { duration: '20s', target: 50 },
+    { duration: '10s', target: 20 }
+    // 100 users
+    // { duration: '10s', target: 75 },
     // { duration: '20s', target: 100 },
     // { duration: '20s', target: 100 },
-    // { duration: '10s', target: 75 }
-    // 200 users
-    // { duration: '10s', target: 100 },
-    // { duration: '20s', target: 200 },
-    // { duration: '20s', target: 200 },
+    // { duration: '10s', target: 50 }
+    // 250
+    // { duration: '10s', target: 200 },
+    // { duration: '20s', target: 250 },
+    // { duration: '20s', target: 250 },
     // { duration: '10s', target: 150 }
     // 500
-    { duration: '10s', target: 300 },
-    { duration: '20s', target: 400 },
-    { duration: '20s', target: 400 },
-    { duration: '10s', target: 350 }
+    // { duration: '10s', target: 300 },
+    // { duration: '20s', target: 400 },
+    // { duration: '20s', target: 400 },
+    // { duration: '10s', target: 350 }
   ]
 }
 
@@ -38,10 +39,10 @@ export const options = {
 
 export default function () {
   const baseUrl = 'http://visipay.syariif-dev.com/api'
-  const transactionId = '00007404-cbc1-44e7-ab6f-ab21cd1135c0'
+  const transactionId = '0010a572-aa73-43e2-9b13-88b72f1f5791'
   const url = baseUrl + '/transaction?transaction_id=' + transactionId
 
-  const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNDA3ZjE3NmUtYTU1MS00NzQyLWFkYjktZjNiNGIxOTE1ZDVhIiwibmFtZSI6IlN5YXJpaWYgQWJkIiwiZW1haWwiOiJzeWFyaWkxZkBtYWlsLmNvbSIsInBob25lIjoiNjI4MjE0MDAwMjg1MSJ9LCJpYXQiOjE2ODUwMTk2NjAsImV4cCI6MTY4NTE5MjQ2MH0.NzDjzIHxXGlrZ1o1USPrQV1ZY8-Ni3sa8OpDJjcIwS8'
+  const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiYTlmOWRiNDUtMmVmOS00MDcyLWFiMDEtNGYzZmRkYzc2Y2I2IiwibmFtZSI6IlN5YXJpaWYgQWJkIiwiZW1haWwiOiJzeWFyaWkxZkBtYWlsLmNvbSIsInBob25lIjoiNjI4MjE0MDAwMjg1MSJ9LCJpYXQiOjE2ODUzNzk4MDMsImV4cCI6MTY4NTU1MjYwM30.ojwGo6W62Qsy6OHt8hr3pqyCO54tBaxqivV05EyoFGA'
 
   const params = {
     headers: {
