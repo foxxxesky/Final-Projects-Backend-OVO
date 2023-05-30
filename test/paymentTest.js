454a835f63a74a95c44a0f25e61ea6b9ff113afc
foxxxesky <abdurrahman1467@gmail.com>
2023-05-30 11:16:33 +0700

    Update paymentTest.js

--- test/paymentTest.js
+++ test/paymentTest.js
@@ -7,26 +7,27 @@ export const options = {
     http_req_failed: ['rate<0.2'] // http errors should be less than 20%
   },
   stages: [
+    // { duration: '1m', target: 50 }
     // 50
-    // { duration: '10s', target: 25 },
-    // { duration: '20s', target: 50 },
-    // { duration: '20s', target: 50 },
-    // { duration: '10s', target: 35 }
-    // 100
-    // { duration: '10s', target: 50 },
+    { duration: '10s', target: 30 },
+    { duration: '20s', target: 50 },
+    { duration: '20s', target: 50 },
+    { duration: '10s', target: 20 }
+    // 100 users
+    // { duration: '10s', target: 75 },
     // { duration: '20s', target: 100 },
     // { duration: '20s', target: 100 },
-    // { duration: '10s', target: 75 }
-    // 200 users
-    // { duration: '10s', target: 100 },
-    // { duration: '20s', target: 200 },
-    // { duration: '20s', target: 200 },
+    // { duration: '10s', target: 50 }
+    // 250
+    // { duration: '10s', target: 200 },
+    // { duration: '20s', target: 250 },
+    // { duration: '20s', target: 250 },
     // { duration: '10s', target: 150 }
     // 500
-    { duration: '10s', target: 300 },
-    { duration: '20s', target: 400 },
-    { duration: '20s', target: 400 },
-    { duration: '10s', target: 350 }
+    // { duration: '10s', target: 300 },
+    // { duration: '20s', target: 400 },
+    // { duration: '20s', target: 400 },
+    // { duration: '10s', target: 350 }
   ]
 }
 
@@ -41,12 +42,12 @@ export default function () {
   const url = baseUrl + '/transaction'
 
   const payload = JSON.stringify({
-    product_id: '521c43a5-8845-4d49-a54b-3e2b098b8d83',
-    promo_id: '38cc1baa-6da6-4514-b6ed-d8bc0c2040d6',
+    product_id: '04a5aca8-dea8-4edc-abf9-69407bd3bf01',
+    promo_id: 'a4826a5b-5657-4ca0-9ab5-b936d3bd01a1',
     notes: 'Isi Pulsa'
   })
 
-  const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNDA3ZjE3NmUtYTU1MS00NzQyLWFkYjktZjNiNGIxOTE1ZDVhIiwibmFtZSI6IlN5YXJpaWYgQWJkIiwiZW1haWwiOiJzeWFyaWkxZkBtYWlsLmNvbSIsInBob25lIjoiNjI4MjE0MDAwMjg1MSJ9LCJpYXQiOjE2ODUxMTM3OTAsImV4cCI6MTY4NTI4NjU5MH0.3__ZWHx0S7Nm5nssRrFuQd2zBwyf0hG4MQxYazU9IW0'
+  const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiYTlmOWRiNDUtMmVmOS00MDcyLWFiMDEtNGYzZmRkYzc2Y2I2IiwibmFtZSI6IlN5YXJpaWYgQWJkIiwiZW1haWwiOiJzeWFyaWkxZkBtYWlsLmNvbSIsInBob25lIjoiNjI4MjE0MDAwMjg1MSJ9LCJpYXQiOjE2ODUzNzk4MDMsImV4cCI6MTY4NTU1MjYwM30.ojwGo6W62Qsy6OHt8hr3pqyCO54tBaxqivV05EyoFGA'
 
   const params = {
     headers: {
